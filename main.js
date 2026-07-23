d3714c306a0104a57f3086803582b8aadb930962
柴田葉奈 <hana@Mac-4.local>
2026-07-23 22:02:15 +0900

    gyro

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 const video = document.getElementById("camera");
 
-navigator.mediaDevices
-    .getUserMedia({
+navigator.mediaDevices.getUserMedia({
+    video: {
+        facingMode: "environment"
+    }
+})
+    .then(stream => {
+        video.srcObject = stream;
+    });
 
-        video: {
-            facingMode: "environment"
+const button = document.getElementById("startAR");
+
+button.addEventListener("click", async () => {
+
+    if (
+        typeof DeviceOrientationEvent !== "undefined" &&
+        typeof DeviceOrientationEvent.requestPermission === "function"
+    ) {
+
+        const permission = await DeviceOrientationEvent.requestPermission();
+
+        if (permission === "granted") {
+
+            window.addEventListener("deviceorientation", (event) => {
+
+                console.log(event.alpha);
+
+            });
+
+            button.style.display = "none";
         }
 
-    })
+    }
 
-    .then(stream => {
-
-        video.srcObject = stream;
-
-    });
+});
